b0fb650be2724571ddab041f788c3699f47ea0bf
VladMaevsky <vlad.maevsky@yandex.ru>
2021-09-19 13:39:52 +0300

    Create project

---
+++ FakeCalls/FakeCalls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeCalls
+//
+//  Created by Vlad Maevsky on 19.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
